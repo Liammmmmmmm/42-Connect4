48425f17023074f9bc76f681cefc410acb4aebc1
Theo Arini <tarini@z4r4p2.42lyon.fr>
2025-09-13 11:14:55 +0200

    start algo

--- include/connect4.h
+++ include/connect4.h
@@ -1,24 +1,32 @@
 #ifndef CONNECT4_H
 # define CONNECT4_H
 
-# include "structs/grid.h"
 # include "structs/game.h"
 
 # define GRID_WIDTH_MIN 7
 # define GRID_HEIGHT_MIN 6
+# define GRID_WIDTH_MAX 100
+# define GRID_HEIGHT_MAX 100
 
 # define N_ALIGN 4
 
-# define GRID_AT(g, x, y) ((g).data[(y) * (g).width + (x)])
+# define GRID_AT(g, x, y) (g->data[y * g->width + x])
 
 # define EMPTY 0
 # define PLAYER 1
 # define BOT -1
 
-int		init_game(t_game *game, char **args);
+int		init_game(t_game *game, int g_width, int g_height);
 
 void	put_usage(char *bin_name);
 
+void	display_grid(t_game *game);
+
 void	destroy_game(t_game *game);
+
+
+// ------ MINIMAX ------ //
+
+int evaluate_board(t_grid grid, int width, int height);
 
 #endif
